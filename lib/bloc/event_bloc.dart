255b547ebd02590f56b23d44a2216268271b671f
Mortti Aittokoski <mortti.aittokoski@aktia.fi>
2020-05-27 18:15:04 +0300

    Added event parsing

--- lib/bloc/event_bloc.dart
+++ lib/bloc/event_bloc.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
-
+import 'dart:convert';
 import 'package:bloc/bloc.dart';
 import 'package:my_app/data/model/event.dart';
-
 import './bloc.dart';
+import 'package:http/http.dart' as http;
 
 class EventBloc extends Bloc<EventEvent, EventState> {
   @override
@@ -15,17 +15,29 @@ class EventBloc extends Bloc<EventEvent, EventState> {
   ) async* {
     if (event is GetEvents) {
       yield EventsLoading();
-      final events = await _fetchEventsFromFakeApi();
+      final events = await _fetchEvents();
       yield EventsLoaded(events);
     }
   }
 
-  Future<List<Event>> _fetchEventsFromFakeApi() {
-    return Future.delayed(
-      Duration(seconds: 2),
-      () {
-        return [Event(name: "name", place: "place", tags: "tags", time: "time")];
-      }
-    );
+  Future<List<Event>> _fetchEvents() async {
+    final response = await http.get('http://api.hel.fi/linkedevents/v1/event/');
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      final data = jsonDecode(response.body);
+
+      var list = data['data'] as List;
+      List<Event> events = list.map((i) => Event.fromJson(i)).toList();
+
+      
+
+      return events;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load events');
+    }
   }
 }
--- lib/data/model/event.dart
+++ lib/data/model/event.dart
@@ -1,4 +1,6 @@
 import 'package:meta/meta.dart';
+import 'package:http/http.dart' as http;
+
 
 class Event {
   final String name;
@@ -12,4 +14,13 @@ class Event {
     @required this.place,
     @required this.tags
   });
+
+  factory Event.fromJson(Map<String, dynamic> json) {
+    var name = json['name'];
+    return Event(
+      name: name['fi'],
+      time: json['start_time'],
+      place: "asdd",
+      tags: "adddd");
+  }
 }
